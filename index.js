df4e7e36848cd98c32c497f28f42494dba625155
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-21 20:05:48 +0530

    Add body-parser

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const config = require("./config/app");
 const router = require("./router");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
+
 const port = config.appPort;
 
 app.listen(port, () => {
--- router/auth.js
+++ router/auth.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
-router.get("/login", (req, res) => {
-  return res.send("Login Screen");
+router.post("/login", (req, res) => {
+  return res.send(["Login", req.body]);
 });
 
-router.get("/register", (req, res) => {
-  return res.send("Register Screen");
+router.post("/register", (req, res) => {
+  return res.send(["Register", req.body]);
 });
 
 module.exports = router;
