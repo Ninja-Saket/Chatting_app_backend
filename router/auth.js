c4766396158d195473fcb1f9c776e93340b4472e
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-22 00:28:52 +0530

    Add form validation for registration

--- router/auth.js
+++ router/auth.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const { login, register } = require("../controllers/authController");
+const { validate } = require("../validators");
+const { body } = require("express-validator");
+const { rules: registrationRules } = require("../validators/auth/register");
 
 router.post("/login", login);
 
-router.post("/register", register);
+router.post("/register", [registrationRules, validate], register);
 
 module.exports = router;
