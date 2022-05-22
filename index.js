181a31ec48b7914dba4d5f94a4120e093cb6df58
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-22 19:37:10 +0530

    Enable cross-origin-resource-sharing

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const config = require("./config/app");
 const router = require("./router");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
