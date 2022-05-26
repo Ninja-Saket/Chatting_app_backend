09a0e25646d3a55dce1801ab61231beb38dde5d6
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-26 09:25:05 +0530

    Create auth middleware

---
+++ router/user.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const { update } = require("../controllers/userController");
+const { validate } = require("../validators");
+const { auth } = require("../middleware/auth");
+router.post("/update", [auth], update);
+
+module.exports = router;
