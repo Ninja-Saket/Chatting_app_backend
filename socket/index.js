3729e6746812d4ff69084257e4784158f5cc925b
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-29 11:25:20 +0530

    Add socket join event

--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,46 @@
 const socketIo = require("socket.io");
+const cors = require("cors");
+
+const users = new Map();
 
 const SocketServer = (server) => {
-  const io = socketIo(server);
-
-  io.on("connection", (socket) => {
+  const io = socketIo(server, { cors: { origin: "*" } });
+  io.on("connect", (socket) => {
     socket.on("join", async (user) => {
-      console.log("New user joined : ", user.firstName);
+      let sockets = [];
+      if (users.has(user.id)) {
+        const existingUser = users.get(user.id);
+        existingUser.sockets = [...existingUser.sockets, ...[socket.id]];
+        users.set(user.id, existingUser);
+        sockets = [...existingUser.sockets, ...[socket.id]];
+      } else {
+        users.set(user.id, { id: user.id, sockets: [socket.id] });
+        sockets.push(socket.id);
+      }
+
+      const onlineFriends = [];
+
+      const chatters = []; //query
+
+      // Notify his friends that user is online
+      for (let i = 0; i < chatters.length; i++) {
+        if (users.has(chatters[i])) {
+          const chatter = users.get(chatters[i]);
+          chatter.sockets.forEach((socket) => {
+            try {
+              io.to(socket).emit("online", user);
+            } catch (error) {}
+          });
+          onlineFriends.push(chatter.id);
+        }
+      }
+
+      // Send to user sockets which of his friends are online
+      sockets.forEach((socket) => {
+        try {
+          io.to(socket).emit("friends", onlineFriends);
+        } catch (error) {}
+      });
     });
   });
 };
