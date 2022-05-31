d73232d83ab00a172e5cb7b12e3aae9ec6702de8
Saket Kumar Ray <phaser.84059@gmail.com>
2022-05-31 15:31:35 +0530

    Search friends on backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,3 +25,34 @@ exports.update = async (req, res) => {
   }
   return res.send("User Controller");
 };
+
+exports.search = async (req, res) => {
+  try {
+    const user = await User.findAll({
+      where: {
+        [sequelize.Op.or]: {
+          namesConcated: sequelize.where(
+            sequelize.fn(
+              "concat",
+              sequelize.col("firstName"),
+              " ",
+              sequelize.col("lastName")
+            ),
+            {
+              [sequelize.Op.iLike]: `%${req.query.term}%`,
+            }
+          ),
+          email: {
+            [sequelize.Op.iLike]: `%${req.query.term}%`,
+          },
+        },
+        [sequelize.Op.not]: {
+          id: req.user.id,
+        },
+      },
+      limit: 10,
+    });
+  } catch (err) {
+    return res.status(500).json({ error: e.message });
+  }
+};
